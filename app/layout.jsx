74f8b85981e4bae0adc27df15ae22713125efb4d
Parth Gupta <parthgupta211206@gmail.com>
2025-07-23 22:42:07 +0530

    update font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,6 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-// import ParticleBackground from "./components/ParticleBackground";
+import ParticleBackground from "./components/ParticleBackground";
 import Footer from "./footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Live Clipboard",
@@ -13,14 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black`}>
-        {/* The z-10 ensures all content renders on top of the particles */}
-        <div className="relative z-10 min-h-screen flex flex-col">
-          <main className="flex-grow">
-            {children}
-          </main>
-          <Footer/>
-        </div>
+      <body>
+        <ParticleBackground />
+        <main className="relative z-10">
+          {children}
+        </main>
+        <Footer/>
       </body>
     </html>
   );
